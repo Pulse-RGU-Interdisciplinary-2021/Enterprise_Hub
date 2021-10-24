f0ea5e917c433491a5e4f8bfac5ec8345acc62a6
mbrougham92 <m.brougham1@rgu.ac.uk>
2021-10-24 20:16:01 +0100

    Created AdminCalendar.js

---
+++ Project/public/scripts/AdminCalendar.js
@@ -0,0 +1,6 @@
+//Microsoft Outlook Calendar/Booking API usage
+//Test Purposes
+//Objectives
+//Add Live Room/Seat Availability
+//Pass through API to book
+//Use Calendar Integrations for site Display/Emails for booking
