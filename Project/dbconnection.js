7aa08c126b7ae0b8d5efe519834fb50b3fec298e
Jon Ander <jonberoz2000@gmail.com>
2021-11-07 11:08:02 +0000

    minor changes on eventBooking

--- Project/dbconnection.js
+++ Project/dbconnection.js
@@ -5,7 +5,7 @@ class DBConnection {
             return await mssql.connect({
                 user: 'sa',
                 password: 'P@55w0rd',
-                server: '84.71.208.86',
+                server: '84.71.233.120',
                 database: 'EIGBooking',
                 options: {
                     trustServerCertificate: true
