655b93dbc31714fc04739721f443bdef4e650e94
Percy <percylackgren@gmail.com>
2021-11-20 18:55:17 +0000

    User data pulled into form, fixed dates in roomdtl

--- Project/public/scripts/room.js
+++ Project/public/scripts/room.js
@@ -1,5 +1,6 @@
 var roomData
 var bookingData
+var userData
 var roomId
 var selectedSeats = []
 
@@ -21,35 +22,74 @@ $(document).ready(function(){
       endDateTime = endDateTime.toISOString().slice(0, 19).replace('T', ' ') + ".000"
 
       $('#book').on('click', function() {
-        $.ajax({
-            type: "POST",
-            url: "/api/v1/bookings/",
-            data: { 
-                "room_id": roomId,
-                "user_id": 1,
-                "start_datetime": startDateTime,
-                "end_datetime": endDateTime,
-                "desks": 1,
-                "reason": $('#reason').val(),
-                "full_room_booking": $("#fullRoomBooking").prop('checked'),
-                "confirmed": 0,
-                "pending": 0 ,
-                "seat_id": selectedSeats[0],
-                "event_booking_yn": 0,
-                "user_name": $('#name').val(),
-                "user_email": $('#email').val(),
-                "user_number": $('#number').val(),
-                "user_role": $('#role').val()
-            },
-            success: function(msg){
-                window.location = "/success";
-            },
-            fail: function(){
-                window.location = "failure";
-            }
-        })
+        
+        if(userId =! ""){
+            $.ajax({
+                type: "POST",
+                url: "/api/v1/bookings/",
+                data: { 
+                    "room_id": roomId,
+                    "user_id": userId,
+                    "start_datetime": startDateTime,
+                    "end_datetime": endDateTime,
+                    "desks": 1,
+                    "reason": $('#reason').val(),
+                    "full_room_booking": $("#fullRoomBooking").prop('checked'),
+                    "confirmed": 0,
+                    "pending": 0 ,
+                    "seat_id": selectedSeats[0],
+                    "event_booking_yn": 0,
+                    "user_name": $('#name').val(),
+                    "user_email": $('#email').val(),
+                    "user_number": $('#number').val(),
+                    "user_role": $('#role').val()
+                },
+                success: function(msg){
+                    window.location = "/success";
+                },
+                fail: function(){
+                    window.location = "/failure";
+                }
+            })
+        } else {
+            alert("User is not logged in!")
+            window.location = "/login";
+        }
     })
 });
+
+async function getData() {
+    roomData = await getRoomInfo(roomId)
+    bookingData = await getRoomBookings(roomId,startDateTime,endDateTime)
+    userData = await getUserById(userId)
+    buildSeatsTable(bookingData, roomData)
+    populateUserInfo(userData)
+}
+
+async function getRoomBookings(room_id, startDateTime, endDateTime) {
+    var output
+    alert("/api/v1/bookings/ByIdAndDateRange/" + room_id + "/" + startDateTime + "/" + endDateTime)
+    await $.get("/api/v1/bookings/ByIdAndDateRange/" + room_id + "/" + startDateTime + "/" + endDateTime, await function (data) {
+        output = data
+    });
+    return output
+}
+
+async function getRoomInfo(room_id) {
+    var output
+    await $.get('/api/v1/rooms/Id/' + room_id, await function(data) {
+        output = data
+    });
+    return output
+}
+
+async function getUserById(userId) {
+    var output
+    await $.get('/api/v1/users/Id/' + userId, await function(data) {
+        output = data
+    }); 
+    return output
+}
 
 async function buildSeatsTable(bookingInput, roomInput) {
     var bookedSeats = [];
@@ -73,30 +113,12 @@ async function buildSeatsTable(bookingInput, roomInput) {
     }
     outputTable += "</tr> </table><br><label for='fullRoom'>Full room booking<input id='fullRoomBooking' name='fullRoom' type='checkbox'></label>"
 
-    console.log(outputTable)
     $('#seats').append(outputTable)
-
     $('#roomId').text(roomInput[0].room_name)
 }
 
-async function getData() {
-    roomData = await getRoomInfo(roomId)
-    bookingData = await getRoomBookings(roomId,startDateTime,endDateTime)
-    buildSeatsTable(bookingData, roomData);
-}
-
-async function getRoomBookings(room_id, startDateTime, endDateTime) {
-    var output
-    await $.get("/api/v1/bookings/ByIdAndDateRange/" + room_id + "/" + startDateTime + "/" + endDateTime, await function (data) {
-        output = data
-    });
-    return output
-}
-
-async function getRoomInfo(room_id) {
-    var output
-    await $.get('/api/v1/rooms/Id/' + room_id, await function(data) {
-        output = data
-    });
-    return output
+async function populateUserInfo(data) {
+    $('#name').val(data[0].full_name)
+    $('#email').val(data[0].email)
+    $('#number').val(data[0].phone_number)
 }
